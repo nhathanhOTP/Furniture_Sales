586b497f1dde9bcf6396a1bceccee9c0217e92f7
nhathanhOTP <nhavtps16500@fpt.edu.vn>
2022-11-11 00:30:08 +0700

    Commit
    Update file!

--- File_Application/BE_Hmart/src/main/java/com/nhathanh/dao/ProductDAO.java
+++ File_Application/BE_Hmart/src/main/java/com/nhathanh/dao/ProductDAO.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 
 import com.nhathanh.entity.Product;
 
-public interface ProductDAO extends JpaRepository<Product, String> {
+public interface ProductDAO extends JpaRepository<Product, Integer> {
 	@Query("SELECT p FROM Product p WHERE p.category.id=?1")
 	List<Product> findByCategoryId(String id);
 	
--- File_Application/BE_Hmart/src/main/java/com/nhathanh/entity/Product.java
+++ File_Application/BE_Hmart/src/main/java/com/nhathanh/entity/Product.java
@@ -34,6 +34,7 @@ public class Product  implements Serializable{
 	@Column(name = "createdate")
 	private Date createDate = new Date();
 	private Boolean available;
+	private String origin;
 	@Column(name = "guarentee")
 	private String guarentee;
 	@Column(name="madeof")
